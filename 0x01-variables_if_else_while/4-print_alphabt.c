f71e2c6769cc6762eb640ba19f0b8296acd8d5a4
Akuthi April <akuthiapril99@gmail.com>
2023-02-17 14:35:52 -0800

    alphabets ommiting a few characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return - outputs alphabets excluding q and e in lowercase
+ */
+
+int main(void)
+{
+	int lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower == 'e' || lower == 'q')
+		{
+			continue;
+		}
+		putchar(lower);
+	}
+	putchar('\n');
+
+	return (0);
+}
