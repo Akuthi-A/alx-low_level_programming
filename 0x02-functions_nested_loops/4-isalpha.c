1fc6e8f26a0a90ec523bf346d2ddf91f47956ac4
Akuthi April <akuthiapril99@gmail.com>
2023-02-21 13:37:41 -0800

    Checking if a character is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - determines whether a character is uppercase or not
+ * _isalpha - determines whether a character is a letter or not
  * @c: the character to be checked
  *
- * Return: returns 1 if c is uppercase, else returns 0
+ * Return: returns 1 if c is letter, else returns 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
 	{
 		return (1);
 	}
