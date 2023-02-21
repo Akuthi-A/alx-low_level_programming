60ca4d2a61e2df94fb26586bd1f8a0bf28e61ad9
Akuthi April <akuthiapril99@gmail.com>
2023-02-21 14:20:51 -0800

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - returns whether a number is +ve or -ve
+ * @n: the number to be checked
+ *
+ * Return: returns 1 if +ve, -1 if -ve, 0 if n==0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
