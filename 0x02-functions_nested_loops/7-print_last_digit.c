d303e0185a61ea8b4705f7a81f8b680d342dcc1c
Akuthi April <akuthiapril99@gmail.com>
2023-02-21 15:04:58 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @num: input number
+ *
+ * Return: returns the last digit of num
+ */
+
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	return (last_digit);
+}
