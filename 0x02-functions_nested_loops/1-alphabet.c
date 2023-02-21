07dd7ed52307afce6e9e415744d4e69ee47ba2d1
Akuthi April <akuthiapril99@gmail.com>
2023-02-21 12:33:43 -0800

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ *
+ * Return: has no return value(void)
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
