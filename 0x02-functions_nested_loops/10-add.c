bddf1ec5e260158bcdd58f71eb35f11e14838cc1
Akuthi April <akuthiapril99@gmail.com>
2023-02-21 16:09:39 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: number to be added
+ * @n2: number to be added
+ *
+ * Return: returns the sum of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
