dafd99d8a6c935b36fa9d7be10de2c7a821f05d8
Akuthi April <akuthiapril99@gmail.com>
2023-02-27 09:11:20 -0800

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - determines the length of a string
+ * @s: string to be checked
+ *
+ * Return: returns the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
