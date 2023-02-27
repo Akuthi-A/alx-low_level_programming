ddafe38ee1efe241cac91af9851276bb0380194c
Akuthi April <akuthiapril99@gmail.com>
2023-02-27 07:53:02 -0800

    dereferncing through a function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an int variable using a pointer to int
+ * @n: pointer to int to be used to update an int variable
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
