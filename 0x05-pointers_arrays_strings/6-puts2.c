21c33993c6fdbbd6570b82f2deb5c3c1ff80d8e6
Akuthi April <akuthiapril99@gmail.com>
2023-02-27 16:36:43 -0800

    every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char in a string with a newline
+ * @str: string to printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	for (; *str != '\0'; str + 2)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
