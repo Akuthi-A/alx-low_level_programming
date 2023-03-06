e8d8d68f05c2c5ee337763ee9645dbd727f5f523
Akuthi April <akuthiapril99@gmail.com>
2023-03-06 13:37:01 -0800

    pointer2pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets a value of a pointer to a char
+ * @s: pointer-to-pointer
+ * @to: pointer
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
