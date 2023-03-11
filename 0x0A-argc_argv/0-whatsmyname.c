bb7208266cf8425bb968fcc24a2cc4ecac12719c
Akuthi April <akuthiapril99@gmail.com>
2023-03-10 16:56:49 -0800

    oh nana whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+
+	return (0);
+}
