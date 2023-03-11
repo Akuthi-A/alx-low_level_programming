57aabe7757f6fc7005afd64e84d49ac271859391
Akuthi April <akuthiapril99@gmail.com>
2023-03-10 17:06:44 -0800

    printing all the arguemnts passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
