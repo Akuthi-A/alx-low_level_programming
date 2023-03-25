37a0b35c701e2e438cca6cc92017bc3e01805d72
Akuthi April <akuthiapril99@gmail.com>
2023-03-24 19:26:28 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
+{
+	int (*prt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	prt = get_op_func(argv[2]);
+
+	if (!prt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", prt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
