b1cde24e43f172995687baf58732cb8de2bdef5e
Akuthi April <akuthiapril99@gmail.com>
2023-03-31 08:27:25 -0700

    printing elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			/**h->len = 0;
+			h->str = "(nil)";*/
+			printf("[0](nil)\n");
+			count++;
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
