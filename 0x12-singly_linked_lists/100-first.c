7c0a3cfab25501bc151dd571c36d708575cf23fd
Akuthi April <akuthiapril99@gmail.com>
2023-03-31 09:23:39 -0700

    printing before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@
 
 void __attribute__ ((constructor)) my_constructor()
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
