9e3e4d9978929c5056ad536ee6d498a2586baad8
Akuthi April <akuthiapril99@gmail.com>
2023-04-04 18:19:38 -0700

    freeing nodes of linkked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees linked list
+ * @head: linked list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
