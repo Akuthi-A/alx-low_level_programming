cd66c6ec966e71cb5f6717ae591f6c47c7a331ea
Akuthi-A <akuthiapril99@gmail.com>
2023-06-07 13:37:43 -0700

    da same back n forth?

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -34,3 +34,20 @@ int check_palindrome(char *s, int start, int end)
 
 	return (check_palindrome(s, start + 1, end - 1));
 }
+
+/**
+ * _strlen_recursion - determines length of a string using recursion
+ * @s: input string
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
