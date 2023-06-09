f413085fde600f4d2d1b10208fd83f86f6c9ac4f
Akuthi-A <akuthiapril99@gmail.com>
2023-06-09 16:59:36 -0700

    static lib

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,2 +1,25 @@
-int _abs(int n);
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of a number
+ * @num: num is the input number
+ *
+ * Return: returns the absolute value of num
+ */
+
+int _abs(int num)
+{
+	int abs_val;
+
+	if (num < 0)
+	{
+		abs_val = num * -1;
+	}
+	else
+	{
+		abs_val = num;
+	}
+
+	return (abs_val);
+}
 
