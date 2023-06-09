d54b397dcfe0c231b438c9995feffc614d0ac29f
Akuthi-A <akuthiapril99@gmail.com>
2023-06-09 04:42:25 -0700

    Command Line Argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return:  0 if inputs are > 2, else return 1
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, result = 1;
+
+	if (argc < 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result *= atoi(argv[i]);
+		}
+	}
+
+	printf("%d \n", result);
+
+	return (0);
+}
