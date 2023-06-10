ea4136d7ff99c093a94c21373267500234850d67
Akuthi-A <akuthiapril99@gmail.com>
2023-06-09 17:20:43 -0700

    static lib

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,31 @@
-char *_strcat(char *dest, char *src);
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates two strings
+ * @dest: String input
+ * @src: String input
+ *
+ * Return: newly concatenated dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int dest_len = 0, src_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (src[src_len] != '\0')
+	{
+		dest[dest_len] = src[src_len++];
+		dest_len++;
+	}
+
+	dest[dest_len++] = '\0';
+
+	return (dest);
+
+}
