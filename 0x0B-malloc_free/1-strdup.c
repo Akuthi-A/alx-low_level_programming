76ec2a32185c3a317f86c20f2debb29ff7d8a71c
Akuthi-A <akuthiapril99@gmail.com>
2023-06-13 04:55:30 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate input string
+ * @str: input string
+ *
+ * Return: pointer to duplicated string, else NULL
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, len = 0;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len += 1;
+
+	duplicate = (char *)malloc(sizeof(char) * len);
+
+	for (i = 0; i < len; i++)
+		duplicate[i] = str[i];
+
+	return (duplicate);
+}
