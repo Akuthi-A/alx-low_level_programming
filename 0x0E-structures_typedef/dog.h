b964ee3acaa57564f4ca17210842e5579c16879c
Akuthi-A <akuthiapril99@gmail.com>
2023-06-19 15:16:21 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - blueprint for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: creator
+ *
+ * Description: men's best friend 1
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
