9dbf07c547b10d59615845f7a4635deb373044f3
Akuthi-A <akuthiapril99@gmail.com>
2023-06-19 15:54:33 -0700

    woof woof

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * void init_dog - initializes a type struct dog
+ * @dog: struct dog
+ * @name: name of dog
+ * @age: age of age
+ * @owner: creator
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+}
