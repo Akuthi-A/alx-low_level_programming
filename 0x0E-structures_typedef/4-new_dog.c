ade46714648cac01ad0c783b142c8097e15c0be5
Akuthi-A <akuthiapril99@gmail.com>
2023-06-19 18:35:49 -0700

    woof woof

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * new_dog - creating a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: creator
+ *
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *snoop;
+
+	snoop = malloc(sizeof(dog_t));
+	if (snoop == NULL)
+		return (NULL);
+
+	snoop->name = name;
+	snoop->age = age;
+	snoop->owner = owner;
+
+	return (snoop);
+}
+
+
+
