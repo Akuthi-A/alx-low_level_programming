10f569af0a1018c22d92f938e6ae8714f3b13169
Akuthi-A <akuthiapril99@gmail.com>
2023-06-19 18:17:47 -0700

    woof woof

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,5 +10,16 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
 }
