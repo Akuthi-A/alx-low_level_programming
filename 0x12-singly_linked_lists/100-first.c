7975b0b3b1c04b704c3250fbfa9b558500cf442b
Akuthi <akuthiapril99@gmail.com>
2023-06-29 14:48:37 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - executes before the main
+ *
+ * Return: void
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
