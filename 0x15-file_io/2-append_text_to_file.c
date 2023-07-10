d692f44be35e993079981b43a93c37549876a9b4
Akuthi <akuthiapril99@gmail.com>
2023-07-10 14:52:04 -0700

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  * Return: 1 on success, else -1
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, numwritten;
 
