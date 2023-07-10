6d7508ba1463ed4b098ef00fffc42b84f4f60677
Akuthi <akuthiapril99@gmail.com>
2023-07-10 10:14:42 -0700

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads textfile onto console
+ * @filename: filename
+ * @letter: num of letters to be read
+ *
+ * Return: number of bytes successfully read, else 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read;
+	char *buffer;
+
+	buffer = (char *)malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+
+	close(fd);
+
+	return (bytes_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
