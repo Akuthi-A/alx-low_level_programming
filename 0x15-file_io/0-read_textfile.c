f44ad360a01ef4b2c97b98beee01c6d5af03812f
Akuthi <akuthiapril99@gmail.com>
2023-07-10 18:10:01 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	bytes_read = read(fd, buffer, letters);
 
-	bytes_written = write(STDOUT_FILENO, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1)
 		return (0);
 
