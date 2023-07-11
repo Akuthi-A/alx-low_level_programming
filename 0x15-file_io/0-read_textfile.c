b46b8d5c16da327faa4bd2fc971e56afeaba2ae7
Akuthi <akuthiapril99@gmail.com>
2023-07-10 18:08:39 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, bytes_read;
+	int fd, bytes_read, bytes_written;
 	char *buffer;
 
 	buffer = (char *)malloc(sizeof(char) * letters);
@@ -25,6 +25,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytes_read = read(fd, buffer, letters);
+
+	bytes_written = write(STDOUT_FILENO, buffer, letters);
+	if (bytes_written == -1)
+		return (0);
 
 	close(fd);
 	free(buffer);
