c6ce507627269b59d3176f99e57eb183bb78d978
Akuthi <akuthiapril99@gmail.com>
2023-08-17 15:27:21 -0700

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums all data in linked list
+ * @head: head
+ *
+ * Return: sum of all the data, else 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	dlistint_t *temp;
+
+	sum = 0;
+	temp = head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (temp != NULL)
+		{
+			sum += temp->n;
+		}
+	}
+	return (sum);
+}
