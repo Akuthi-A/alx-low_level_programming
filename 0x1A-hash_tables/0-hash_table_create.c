372ae8960ec6cce85fd1f4f5609931154c801361
Akuthi <akuthiapril99@gmail.com>
2023-09-07 14:07:53 -0700

    hashing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,14 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	/* Allocate memory for hash table structure */
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	/* Allocate memory for array of hash nodes */
+
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
@@ -34,7 +34,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 
-	/* Initialize all hash node pointers to NULL */
 	for (i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
